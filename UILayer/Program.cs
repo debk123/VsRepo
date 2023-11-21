67e7991b1e0bb871eed1af86e4a6ecb5f92c5457
raj <raj@gmail.com>
2023-11-21 15:15:09 +0530

    firstcommit

---
+++ UILayer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UILayer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to Devops tool");
+        }
+    }
+}
